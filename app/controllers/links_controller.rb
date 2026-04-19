36141ef226d40f60ab807a8fff87254bcab74a42
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-04-19 16:28:28 -0600

    Poll link scraping jobs from the links UI

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,7 +4,7 @@ class LinksController < ApplicationController
   def index
     @filter = params[:filter] == "read" ? "read" : "unread"
 
-    scope = Link.order(updated_at: :desc).select(:id, :url, :note, :read, :timestamp, :updated_at)
+    scope = Link.includes(:content_jobs).order(updated_at: :desc).select(:id, :url, :note, :read, :timestamp, :updated_at, :content)
     @links = scope.to_a
 
     @read_links_count = @links.count { |link| link.read.to_i == 1 }
