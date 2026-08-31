547bf12f1ee0c62c8c9c7ca518fb810575d98bd5
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-08-31 13:10:51 -0600

    Fix log size ./serve error

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ require "action_view/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+require "active_support/core_ext/numeric/bytes"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
